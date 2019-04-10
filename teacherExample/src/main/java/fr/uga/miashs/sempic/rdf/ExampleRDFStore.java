020110fc5d67430dfbc617e41b8b6b7e5d5768f6
JulioJu <juanes0890@gmail.com>
2019-04-10 17:58:56 +0200

    teacherExample: improve clarity of the example

--- teacherExample/src/main/java/fr/uga/miashs/sempic/rdf/ExampleRDFStore.java
+++ teacherExample/src/main/java/fr/uga/miashs/sempic/rdf/ExampleRDFStore.java
@@ -12,14 +12,31 @@ import org.apache.jena.vocabulary.RDFS;
  * @author Jerome David <jerome.david@univ-grenoble-alpes.fr>
  */
 public class ExampleRDFStore {
+
+    public static void queries(RDFStore rdfStore) {
+
+        System.out.println("\n\nStart of queries\n————————————");
+        List<Resource> classes = rdfStore.listSubClassesOf(SempicOnto.Depiction);
+        classes.forEach(c -> {System.out.println(c);});
+
+        List<Resource> instances = rdfStore.createAnonInstances(classes);
+        instances.forEach(i -> {
+            System.out.println(i.getProperty(RDFS.label));
+        });
+        System.out.println("End of queries\n————————————");
+    }
+
     public static void main(String[] args) {
 
-        System.out.println("Start of app");
+        System.out.println("\n\n\nStart of app\n————————————\n\n");
+
 
         RDFStore rdfStore = new RDFStore();
 
+        queries(rdfStore);
+
         // ————————————————————————————
-        System.out.println("Coucou 0");
+        System.out.println("\n\nStart of create triples\n————————————");
 
         Resource photoResource = rdfStore.createPhoto(1, 1, 1);
         Model model = ModelFactory.createDefaultModel();
@@ -40,24 +57,20 @@ public class ExampleRDFStore {
 
         // Print
         model.write(System.out, "turtle");
-        System.out.println("Coucou 2");
+        System.out.println("End of create triples\n————————————\n\n");
         // ————————————————————————————
 
         rdfStore.saveModel(model);
 
         //rdfStore.deleteModel(model);
         //rdfStore.cnx.load(model);
-        List<Resource> classes = rdfStore.listSubClassesOf(SempicOnto.Depiction);
-        classes.forEach(c -> {System.out.println(c);});
-
-        List<Resource> instances = rdfStore.createAnonInstances(classes);
-        instances.forEach(i -> {
-            System.out.println(i.getProperty(RDFS.label));
-        });
+        queries(rdfStore);
 
         //rdfStore.deleteModel(model);
         //rdfStore.readPhoto(1).getModel().write(System.out,"turtle");
         // print the graph on the standard output
         //photoResource.getModel().write(System.out);
+
+        System.out.println("\n\n\nEnd of app\n————————————\n\n");
     }
 }
